7c8fdafa7e42e60cc0d6c28bace414bdc6cca592
Charles Jones <cj1917@ic.ac.uk>
2022-12-01 16:51:02 +0000

    finish day 1

--- aoc-2022-01/src/main.rs
+++ aoc-2022-01/src/main.rs
@@ -6,7 +6,6 @@ fn main() {
     let config = parse_config(&args);
     let contents =
         fs::read_to_string(config.file_path).expect("Something went wrong reading the file");
-    println!("With text:\n{contents}");
 
     if config.question == "a" {
         let result = solve_part_a(&contents);
@@ -35,10 +34,39 @@ fn parse_config(args: &[String]) -> Config {
     }
 }
 
-fn solve_part_a(contents: &String) -> i32 {
-    0
+// Very simple imperative solution. Loop through lines, keeping track of the current
+// score and the best score seen so far.
+fn solve_part_a(contents: &str) -> i32 {
+    let mut best = 0;
+    let mut current = 0;
+    for line in contents.lines() {
+        // empty lines delineate the different elves
+        if line.is_empty() {
+            if current > best {
+                best = current;
+            }
+            current = 0;
+        } else {
+            current += line.parse::<i32>().unwrap();
+        }
+    }
+    best
 }
 
-fn solve_part_b(contents: &String) -> i32 {
-    0
+// Convert to list of summed scores, then sort and sum first 3.
+fn solve_part_b(contents: &str) -> i32 {
+    let mut scores: Vec<i32> = Vec::new();
+    let mut current = 0;
+    for line in contents.lines() {
+        // empty lines delineate the different elves
+        if line.is_empty() {
+            scores.push(current);
+            current = 0;
+        } else {
+            current += line.parse::<i32>().unwrap();
+        }
+    }
+    scores.sort_unstable();
+    let len = scores.len();
+    scores[len - 1] + scores[len - 2] + scores[len - 3]
 }
