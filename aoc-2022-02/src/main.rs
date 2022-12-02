cc170e7a5b7cfc20252fd0a28158089d9e69371b
Charles Jones <cj1917@ic.ac.uk>
2022-12-02 10:31:19 +0000

    add input file

--- aoc-2022-02/src/main.rs
+++ aoc-2022-02/src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let configs = Config::from_args(&args);
+    let contents = configs.get_contents();
+
+    let inputs = parse_contents(&contents);
+
+    match configs.question.as_str() {
+        "a" => println!("Answer to Part a: {}", solve_part_a(&inputs)),
+        "b" => println!("Answer to Part b: {}", solve_part_b(&inputs)),
+        _ => println!("Question must be a or b, got {}", configs.question),
+    }
+}
+
+struct Config {
+    question: String,
+    file_path: String,
+}
+
+impl Config {
+    fn from_args(args: &[String]) -> Config {
+        let question = args[1].clone();
+        let file_path = args[2].clone();
+        Config {
+            question,
+            file_path,
+        }
+    }
+
+    fn get_contents(&self) -> String {
+        fs::read_to_string(&self.file_path).expect("The file path should be valid")
+    }
 }
