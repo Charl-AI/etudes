8797bb261c00d69ff9a9056f91b12b674c52e2ff
C Jones <cj1917@ic.ac.uk>
2023-05-28 15:21:10 +0100

    solve lua 2021 day 1

--- lua/README.md
+++ lua/README.md
@@ -1,3 +1,7 @@
 # Lua
 
 We solve the 2021 problems in lua. We use lua 5.4.6. Note that this is slightly different to the version neovim uses (5.1).
+
+## Basics
+
+Run scripts with `lua main.lua a input.txt` (change a to b for part b).
---
+++ lua/aoc-2021-01/main.lua
@@ -0,0 +1,57 @@
+local function parse_args()
+  local question = arg[1]
+  local filepath = arg[2]
+
+  if question == nil or (question ~= 'a' and question ~= 'b') then
+    print("Invalid question. The first CLI argument must be either 'a' or 'b'.")
+    os.exit(1)
+  end
+
+  if filepath == nil then
+    print("Invalid filepath. The second CLI argument must be a valid filepath.")
+    os.exit(1)
+  end
+
+  return question, filepath
+end
+
+local function solve_part_a(m)
+  local num_increases = 0
+  for i = 2, #m do
+    if m[i] > m[i - 1] then
+      num_increases = num_increases + 1
+    end
+  end
+  return num_increases
+end
+
+-- the trick here is that to check if the sum of a sliding window increases,
+-- we only need to check if the number coming in is greater than the number
+-- going out.
+local function solve_part_b(m)
+  local num_increases = 0
+  for i = 4, #m do
+    if m[i] > m[i - 3] then
+      num_increases = num_increases + 1
+    end
+  end
+  return num_increases
+end
+
+local question, filepath = parse_args()
+
+-- measurements are lines in the input file
+-- here we read into a table/list
+local measurements = {}
+
+for line in io.lines(filepath) do
+  table.insert(measurements, tonumber(line))
+end
+
+print("Loaded " .. #measurements .. " measurements from " .. filepath)
+
+if question == 'a' then
+  print(solve_part_a(measurements))
+else
+  print(solve_part_b(measurements))
+end
